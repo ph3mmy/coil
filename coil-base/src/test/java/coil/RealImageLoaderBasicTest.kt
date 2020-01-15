766318b6c136fba968aed3d6756c0f2bcd1d4731
Colin White <colin@colinwhite.me>
2020-01-15 00:06:23 -0800

    Fix tests.

--- coil-base/src/test/java/coil/RealImageLoaderBasicTest.kt
+++ coil-base/src/test/java/coil/RealImageLoaderBasicTest.kt
@@ -24,7 +24,6 @@ import coil.size.SizeResolver
 import coil.transform.Transformation
 import coil.util.createBitmap
 import coil.util.createGetRequest
-import coil.util.createImageLoader
 import coil.util.createLoadRequest
 import coil.util.error
 import coil.util.toDrawable
@@ -32,6 +31,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.runBlocking
+import okhttp3.OkHttpClient
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -62,11 +62,14 @@ class RealImageLoaderBasicTest {
         bitmapPool = BitmapPool(Long.MAX_VALUE)
         referenceCounter = BitmapReferenceCounter(bitmapPool)
         memoryCache = MemoryCache(referenceCounter, Int.MAX_VALUE)
-        imageLoader = createImageLoader(
-            context = context,
-            bitmapPool = bitmapPool,
-            referenceCounter = referenceCounter,
-            memoryCache = memoryCache
+        imageLoader = RealImageLoader(
+            context,
+            DefaultRequestOptions(),
+            bitmapPool,
+            referenceCounter,
+            memoryCache,
+            OkHttpClient(),
+            ComponentRegistry()
         )
     }
 
